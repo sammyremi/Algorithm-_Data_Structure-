1b19cab32d11db41f4790177c229aa53dc582784
sammyremi <adebayorsamuelas@gmail.com>
2024-08-04 10:51:17 +0100

    remove duplicate

--- Leetcode_150/remove_duplicate_from_sorted_array_2.rb
+++ Leetcode_150/remove_duplicate_from_sorted_array_2.rb
@@ -1,18 +1,6 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def remove_duplicates(nums)
-    # loop through each element in array
-    # check the count of current element
-    # if count greater than or equal 3 change item at that index
-    # delete all item that was changed
-    # return array sorted
-
-    for x in 0..nums.length - 1
-        num_count = nums.count(nums[x])
-        if num_count >= 3
-            nums[x] = ""
-        end
-    end
-   nums.delete("")
-   return nums.count
+    nums.uniq!
+    return nums.count
 end

693c1ebf87874730bd4b3fcf8dd60638c97df3eb
sammyremi <adebayorsamuelas@gmail.com>
2024-08-04 16:38:16 +0100

    remove duplicate from array 2

--- Leetcode_150/remove_duplicate_from_sorted_array.rb
+++ Leetcode_150/remove_duplicate_from_sorted_array.rb
@@ -1,8 +1,21 @@
 # @param {Integer[]} nums
-# @param {Integer} val
 # @return {Integer}
-def remove_element(nums, val)
+def remove_duplicates(nums)
+    # loop through each element with len index
+    # get count of current index from array
+    #check if element more than 2 
+    #if element more than 2 change current element to ""
+    #delete all "" from array
+
     
-    nums.delete(val)
-    return nums.count
+        for x in 0..nums.length - 1
+            ele_count = nums.count(nums[x])
+            if ele_count > 2
+                nums[x] = ""
+            end
+        end
+    
+        nums.delete("")
+        return nums.length
+        
 end

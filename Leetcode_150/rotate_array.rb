c0be176c3057894a7b54821f49293b89beb6683a
sammyremi <adebayorsamuelas@gmail.com>
2024-08-14 15:06:08 +0100

    rotate array second solution

--- Leetcode_150/rotate_array.rb
+++ Leetcode_150/rotate_array.rb
@@ -7,3 +7,23 @@ def rotate(nums, k)
 
     nums.rotate!(-k)
 end
+
+#=====================================================================================================
+# solution 2
+#=====================================================================================================
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+
+    #loop through array from behind
+    #copy last element of array to first
+    #delete last element
+    for x in 1..k 
+        temp = nums[-1]
+        nums.unshift(temp)
+        nums.pop
+    end
+
+end

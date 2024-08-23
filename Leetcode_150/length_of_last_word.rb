37f1bd8a191f415629b1aa4616db37f815fc3de7
sammyremi <adebayorsamuelas@gmail.com>
2024-08-23 20:34:16 +0100

    length of last word comment

--- Leetcode_150/length_of_last_word.rb
+++ Leetcode_150/length_of_last_word.rb
@@ -1,6 +1,9 @@
 # @param {String} s
 # @return {Integer}
 def length_of_last_word(s)
+    # split string with space
+    # return array at last index of return array
+
     str_arr = s.split(" ")
     return str_arr.last.length
 end

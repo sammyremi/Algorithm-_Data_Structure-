9130023e7cad4f32314522ea2ce950c111b4862c
sammyremi <adebayorsamuelas@gmail.com>
2024-08-29 05:52:30 +0100

    refactor decipher text

--- Leetcode_150/decipher.rb
+++ Leetcode_150/decipher.rb
@@ -1,34 +1,40 @@
-def decipher(ciphertext)
-        # get all a - z  in a avarible 
-        # get all A - Z in a variable
-        # create a new string to store new text
-        # loop throuh each char and look for it in both variable
-        # also check for other character
-        # get index and - 2 
-        # append result to new string
+def decipher(ciphertext, check_word)
+    # get all a - z  in a avarible 
+    # get all A - Z in a variable
+    # initialize kount to zero
+    # creaye a while loop to run for alphabeth length
+    # create a new string to store new text
+    # loop throuh each char and look for it in both variable
+    # also check for other character
+    # get index and subtract index from kount
+    # append result to new string
 
-        low_case = ("a".."z").to_a
-        up_case = ("A".."Z").to_a
+    low_case = ("a".."z").to_a
+    up_case = ("A".."Z").to_a
 
+    
+    kount = 0
+    while kount < 26
         new_str = ""
-        flag = false
-
         ciphertext.each_char do |str|
             if low_case.include?(str)
                 idx = low_case.index(str)
-                ans = low_case[idx - 2]
+                ans = low_case[idx - kount]
                 new_str << ans
             elsif up_case.include?(str)
                 idx = up_case.index(str)
-                ans = up_case[idx - 2]
+                ans = up_case[idx - kount]
                 new_str << ans
             else
                 new_str << str
             end
 
         end
-
-        p new_str
+        kount += 1
+        return new_str if new_str.include?(check_word)
+    end
+    return "Invalid"
+    
 end
 
-decipher("Eqfkpi vguvu ctg hwp!")
+p decipher("Eqfkpi vguvu ctg hwp!", "tests")
